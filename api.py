e06d2c353a891d064d6fea5caa4f4136c637f8dd
Bo Shang <cnpcshangbo@gmail.com>
2024-03-26 19:15:03 -0400

    cleaned workspace, to project with pure python library

--- api.py
+++ api.py
@@ -7,6 +7,12 @@ CORS(app)
 @app.route('/api', methods=['GET'])
 def get_fake_number():
     print(request.args)
+    html_file = request.args.get('html_file')
+    print(html_file)
+    html_file_name = html_file.split('/')[-1]
+    print(html_file_name)
+    if html_file_name == 'copc_bayonne_crackoverlay_meta.html':
+        print('find the right las file')
     response = jsonify({'number': 2.456})
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
